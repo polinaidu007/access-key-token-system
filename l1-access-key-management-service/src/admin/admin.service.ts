cb65c85a2e95309ac4d00674552e4907a8b7d853
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2025-05-30 02:19:02 +0530

    add error handling in write operations

--- l1-access-key-management-service/src/admin/admin.service.ts
+++ l1-access-key-management-service/src/admin/admin.service.ts
@@ -1,33 +1,57 @@
 // src/admin/admin.service.ts
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { KeyStoreService } from '../db/key-store.service';
 import { StreamingProducerService } from '../streaming/streaming.producer.service';
-import { StreamingEvent, StreamName } from 'src/constants/streaming.constants';
+import { StreamingEvent, StreamName } from '../constants/streaming.constants';
 import { CreateKeyDto } from './dto/create-key.dto';
+import { UpdateKeyDto } from './dto/update-key.dto';
 
 @Injectable()
 export class AdminService {
+  private readonly logger = new Logger(AdminService.name);
   constructor(
     private readonly keyStore: KeyStoreService,
     private readonly producer: StreamingProducerService,
   ) {}
 
   async createKey(payload: CreateKeyDto) {
+    const logKey = `createKey:`
     const key = payload.key;
-    const existing = await this.keyStore.getAccessKey(key);
+
+    const existing = await this.keyStore.getAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.getAccessKey failed with error`, error.stack, payload);
+      throw new InternalServerErrorException('Failed to create access key');
+    })
     if (existing) throw new ConflictException('Key already exists');
 
-    await this.keyStore.setAccessKey(key, {...payload, enabled : true});
+    // set data in db
+    await this.keyStore.setAccessKey(key, {...payload, enabled : true}).catch(error => {
+      this.logger.error(`${logKey} Failed to set access key`, error.stack, payload);
+      throw new InternalServerErrorException('Failed to create access key');
+    })
+
+    // publish msg to streams
     await this.producer.publish(StreamName.ACCESS_KEY_EVENTS, {
       event: StreamingEvent.KEY_CREATED,
       ...payload,
       enabled : true
+    }).catch(async error => {
+      this.logger.error(`${logKey} Failed to stream access key`, error.stack, payload);
+
+      // revert the previous action in db
+      await this.keyStore.deleteAccessKey(key).catch(err => {
+        this.logger.error(`${logKey} Failed to revert create action`, error.stack, payload);
+      });
+
+      throw new InternalServerErrorException(`Failed to create access key`);
     });
+
+    //return to client
     return { message: 'Key created' };
   }
 
   async getKey(key: string) {
-    return this.keyStore.getAccessKey(key);
+    return await this.keyStore.getAccessKey(key);
   }
 
   async listKeys() {
@@ -36,26 +60,69 @@ export class AdminService {
     return keys || [];
   }
 
-  async updateKey(key: string, newData: any) {
-    const existing = await this.keyStore.getAccessKey(key);
+  async updateKey(key: string, newData: UpdateKeyDto) {
+    const logKey = `updateKey:`
+
+    // fetch existing data for the key from db
+    const existing = await this.keyStore.getAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.getAccessKey failed with error:`, error.stack, key, newData);
+      throw new InternalServerErrorException(`Failed to update data`);
+    })
     if (!existing) throw new NotFoundException('Key not found');
 
+    // set data in database
     const updated = { ...existing, ...newData };
-    await this.keyStore.setAccessKey(key, updated);
+    await this.keyStore.setAccessKey(key, updated).catch(error => {
+      this.logger.error(`${logKey} keyStore.setAccessKey failed with error:`, error.stack, key, newData);
+      throw new InternalServerErrorException(`Failed to update data`);
+    });
+
+    // send data through streams to L2
     await this.producer.publish(StreamName.ACCESS_KEY_EVENTS, {
       event: StreamingEvent.KEY_UPDATED,
       key,
       ...updated,
+    }).catch(error => {
+      this.logger.error(`${logKey} producer.publish failed with error`, error.stack, key, newData);
+      // revert the previous action in db
+      this.keyStore.setAccessKey(key, existing).catch(error => {
+        this.logger.error(`${logKey} keyStore.setAccessKey(inside 2nd catch) failed with error:`, error.stack, key, newData);
+      });
+      throw new InternalServerErrorException(`Failed to update data`);
     });
+
     return { message: 'Key updated' };
   }
 
   async deleteKey(key: string) {
-    await this.keyStore.deleteAccessKey(key);
+    const logKey = `deleteKey:`
+
+    const existing = await this.keyStore.getAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.getAccessKey failed with error:`, error.stack, key);
+      throw new InternalServerErrorException(`Failed to delete key`);
+    })
+
+    if (!existing) throw new NotFoundException('Key not found');
+
+    // delete key from db
+    await this.keyStore.deleteAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.deleteAccessKey failed with error:`, error.stack, key);
+      throw new InternalServerErrorException(`Failed to delete key`);
+    });
+
+    // publish to stream
     await this.producer.publish(StreamName.ACCESS_KEY_EVENTS, {
       event: StreamingEvent.KEY_DELETED,
       key,
-    });
+    }).catch(error => {
+      this.logger.error(`${logKey} producer.publish failed with error`, error.stack, key);
+      // revert the previous action in db
+      this.keyStore.setAccessKey(key, existing).catch(error => {
+        this.logger.error(`${logKey} keyStore.setAccessKey failed with error:`, error.stack, key);
+      });
+      throw new InternalServerErrorException(`Failed to delete key`);
+    })
+    
     return { message: 'Key deleted' };
   }
 }
--- l1-access-key-management-service/src/user/user.service.ts
+++ l1-access-key-management-service/src/user/user.service.ts
@@ -1,33 +1,59 @@
 // src/user/user.service.ts
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { KeyStoreService } from '../db/key-store.service';
-import { StreamingProducerService } from 'src/streaming/streaming.producer.service';
-import { StreamingEvent, StreamName } from 'src/constants/streaming.constants';
+import { StreamingProducerService } from '../streaming/streaming.producer.service';
+import { StreamingEvent, StreamName } from '../constants/streaming.constants';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
   constructor(
     private readonly keyStore: KeyStoreService,
     private readonly producer: StreamingProducerService,
   ) {}
 
   async getKeyInfo(key: string) {
-    const data = await this.keyStore.getAccessKey(key);
+    const logKey = `getKeyInfo:`
+    const data = await this.keyStore.getAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.getAccessKey failed with error:`, error.stack, key);
+      throw new InternalServerErrorException(`Failed to get key info`);
+    })
     if (!data) throw new NotFoundException('Invalid key');
     return data;
   }
 
   async disableKey(key: string) {
-    const data = await this.keyStore.getAccessKey(key);
+    const logKey = `disableKey:`
+    const data = await this.keyStore.getAccessKey(key).catch(error => {
+      this.logger.error(`${logKey} keyStore.getAccessKey failed with error:`, error.stack, key);
+      throw new InternalServerErrorException(`Failed to disable key`);
+    })
     if (!data) throw new NotFoundException('404: Key not found');
+    if(!data.enabled)
+        return { message : `Key already disabled`}
 
+    // update in db
     data.enabled = false;
-    await this.keyStore.setAccessKey(key, data);
+    await this.keyStore.setAccessKey(key, data).catch(error => {
+      this.logger.error(`${logKey} keyStore.setAccessKey failed with error:`, error.stack, key);
+      throw new InternalServerErrorException(`Failed to disable key`);
+    });
+
+    // publish to streams
     await this.producer.publish(StreamName.ACCESS_KEY_EVENTS, {
-          event: StreamingEvent.KEY_UPDATED,
-          key,
-          ...data,
-        });
+      event: StreamingEvent.KEY_UPDATED,
+      key,
+      ...data,
+    }).catch(error => {
+      this.logger.error(`${logKey} producer.publish failed with error`, error.stack, key);
+      // revert the previous action in db
+      data.enabled = true;
+      this.keyStore.setAccessKey(key, data).catch(error => {
+        this.logger.error(`${logKey} keyStore.setAccessKey(in 2nd catch) failed with error:`, error.stack, key);
+      });
+      throw new InternalServerErrorException(`Failed to disable key`);
+    });
+
     return { message: 'Key disabled' };
   }
 }
