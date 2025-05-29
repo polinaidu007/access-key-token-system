34656a430f0eaa4f82e9f128126763aa68799c59
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2025-05-29 16:49:38 +0530

    add swagger, config module, db and streaming modules in app module

--- l1-access-key-management-service/src/app.module.ts
+++ l1-access-key-management-service/src/app.module.ts
@@ -5,10 +5,17 @@ import { AdminController } from './admin/admin.controller';
 import { UserController } from './user/user.controller';
 import { AdminService } from './admin/admin.service';
 import { UserService } from './user/user.service';
+import { StreamingModule } from './streaming/streaming.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true, // makes it available app-wide
+  }), 
+  StreamingModule, DbModule],
   controllers: [AppController, AdminController, UserController],
   providers: [AppService, AdminService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
