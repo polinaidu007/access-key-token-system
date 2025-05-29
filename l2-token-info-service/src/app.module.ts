b090e556f2c2860f3ea8b05159a6da30dd96d766
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2025-05-29 23:51:53 +0530

    add swagger, config, db, streaming modules in app module in l2

--- l2-token-info-service/src/app.module.ts
+++ l2-token-info-service/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { TokenController } from './token/token.controller';
+import { TokenService } from './token/token.service';
+import { ConfigModule } from '@nestjs/config';
+import { StreamingModule } from './streaming/streaming.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true, // makes it available app-wide
+  }), 
+    DbModule,
+    StreamingModule
+  ],
+  controllers: [AppController, TokenController],
+  providers: [AppService, TokenService],
 })
 export class AppModule {}
