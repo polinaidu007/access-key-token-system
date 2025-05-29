cd0c323644e15827ebd9142083e8d4bca0b31fcd
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2025-05-29 23:48:15 +0530

    publish event from disable key api

--- l1-access-key-management-service/src/user/user.service.ts
+++ l1-access-key-management-service/src/user/user.service.ts
@@ -1,10 +1,15 @@
 // src/user/user.service.ts
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { KeyStoreService } from '../db/key-store.service';
+import { StreamingProducerService } from 'src/streaming/streaming.producer.service';
+import { StreamingEvent, StreamName } from 'src/constants/streaming.constants';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly keyStore: KeyStoreService) {}
+  constructor(
+    private readonly keyStore: KeyStoreService,
+    private readonly producer: StreamingProducerService,
+  ) {}
 
   async getKeyInfo(key: string) {
     const data = await this.keyStore.getAccessKey(key);
@@ -18,6 +23,11 @@ export class UserService {
 
     data.enabled = false;
     await this.keyStore.setAccessKey(key, data);
+    await this.producer.publish(StreamName.ACCESS_KEY_EVENTS, {
+          event: StreamingEvent.KEY_UPDATED,
+          key,
+          ...data,
+        });
     return { message: 'Key disabled' };
   }
 }
