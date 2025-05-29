db2e887d1cd5cfaa2f7f136525f57742960b6ddd
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2025-05-29 23:52:09 +0530

    add swagger doc in l2

--- l2-token-info-service/src/main.ts
+++ l2-token-info-service/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Token Validation Service')
+    .setDescription('L2 Service for Access Key Validation and Rate Limiting')
+    .setVersion('1.0')
+    .addTag('token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
